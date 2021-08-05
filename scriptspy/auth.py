649182bbea12be6e3f670e06b5c86b6d97b37c8f
GentleWarrior <82219920+GentleWarrior@users.noreply.github.com>
2021-08-05 21:13:18 +0300

    Print

--- scriptspy/auth.py
+++ scriptspy/auth.py
@@ -48,7 +48,7 @@ def signup_post():
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
+    print(new_user)
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
