2c763d21af288640abb461b7621c229701c55689
GentleWarrior <82219920+GentleWarrior@users.noreply.github.com>
2021-08-06 21:47:10 +0300

    P

--- scriptspy/auth.py
+++ scriptspy/auth.py
@@ -18,20 +18,20 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
+    admin = Admin.query.filter_by(email=email).first()
 
     # check if user actually exists
     # take the user supplied password, hash it, and compare it to the hashed password in database
-    if not user or not check_password_hash(user.password, password):
+    if not admin or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
+    login_admin(admin, remember=remember)
     return redirect(url_for('main.profile'))
 
 
-@auth.route('/signup', methods=['GET', 'POST'])
+@auth.route('/signup_doc', methods=['GET', 'POST'])
 def signup_doc():
     email = ""
     name = ""
@@ -60,30 +60,5 @@ def signup_doc():
 def logout():
     logout_user()
     return redirect(url_for('main.py'))
-
-
-@auth.route('/signup_donor', methods=['GET', 'POST'])
-def signup_donor():
-    email = ""
-    name = ""
-    password = ""
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()  # if this returns a user, then the email already exists in
-    # database
-    if user:  # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup_donor'))
-
-    # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-    print(new_user)
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('views.contact'))
 
 
