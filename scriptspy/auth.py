bac94575ba7280579b2a17604594ea11fabbc3a6
GentleWarrior <82219920+GentleWarrior@users.noreply.github.com>
2021-08-06 22:20:28 +0300

    P

--- scriptspy/auth.py
+++ scriptspy/auth.py
@@ -27,7 +27,7 @@ def login_post():
         return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
-    login_user(admin)
+    login_user(admin, remember=remember)
     return redirect(url_for('main.profile'))
 
 
