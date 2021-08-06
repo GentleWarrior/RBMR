e237d71e37677f81eff357e0fc9cf49118c03d93
GentleWarrior <82219920+GentleWarrior@users.noreply.github.com>
2021-08-06 21:52:06 +0300

    P

--- scriptspy/models.py
+++ scriptspy/models.py
@@ -5,7 +5,9 @@ from flask_login import UserMixin
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
     password = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True)
 
 
 class User(db.Model, UserMixin):
